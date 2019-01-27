65581667a82212a0c9ca6c7144084d6f834fc62a
MrFreesher <artur260@op.pl>
2019-01-27 20:08:09 +0100

    Add frontend to login page

--- public/js/app.js
+++ public/js/app.js
@@ -1,2 +1,20 @@
 const loginBtn = document.querySelector("#loginBtn");
 const usernameField = document.querySelector("#usernameField");
+
+
+function checkLoginExist(){
+    console.log(localStorage.getItem("username"));
+    if(localStorage.getItem("username") !== null){
+        window.location = "/chat";
+    }
+}
+document.addEventListener('DOMContentLoaded', checkLoginExist);
+loginBtn.addEventListener("click",login);
+
+function login(){
+    if(usernameField.value !== ""){
+        localStorage.setItem("username",usernameField.value);
+    }else{
+        console.log("Pole nie może być puste ");
+    }
+}
